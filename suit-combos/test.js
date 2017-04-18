47bf1905df027f8334429dba98c9ed9e5236ddeb
elckhrt <elckhrt@users.noreply.github.com>
2017-04-18 11:39:29 +0100

    Report execution time.

--- suit-combos/test.js
+++ suit-combos/test.js
@@ -8,7 +8,9 @@ function test_1()
   let sc = new core.SuitCombination(core.cards_from_string(north), core.cards_from_string(south));
   let lc = new core.LineChecker(sc);
   let ls = new core.LineSolver(lc);
+  let t0 = performance.now();
   let actual = ls.solve();
+  let t1 = performance.now();
   let expected = [ [ 3, 4, 4, 3, 4, 4, 4, 4, 5, 4, 4, 4 ],
   [ 3, 4, 4, 3, 4, 4, 5, 4, 4, 4, 4, 4 ],
   [ 3, 4, 4, 3, 5, 4, 5, 3, 4, 4, 5, 4 ],
@@ -16,6 +18,7 @@ function test_1()
   [ 3, 4, 4, 3, 4, 4, 5, 4, 4, 5, 3, 5 ],
   [ 3, 4, 4, 3, 5, 4, 5, 4, 3, 4, 5, 4 ] ];
   assert.deepEqual(expected.sort(), actual.sort());
+  console.log('Analysis time: ' + (t1 - t0).toFixed(2) + ' sec')
 }
 
 test_1();
